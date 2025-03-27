41c779a4c3c64c626eea2a1d70a5188c723d3de1
AndreSosa21 <andreasosro@unisabana.edu.co>
2025-03-27 08:14:16 -0500

    first version

--- src/test/java/HolaMundoTest.java
+++ src/test/java/HolaMundoTest.java
@@ -1,1 +1,13 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
  
+public class HolaMundoTest {
+ 
+    @Test
+    void testHolaMundo() {
+        String mensaje = "¡Hola Mundo desde CI/CD!";
+        assertEquals("¡Hola Mundo desde CI/CD!", mensaje);
+    }
+}
